c948d82e474bbbb9965b4a261c1ad8b9bdd7a506
agiratech-lavanyav <lavanya.v@agiratech.com>
2022-05-20 19:09:44 +0530

    Used for loop to find the perimeter (Polygon class)

--- polygon.js
+++ polygon.js
@@ -3,9 +3,19 @@ class Polygon {
         this.values =lengths;
     }
     perimeter(){
-        return (this.values).reduce((num, item) => num + item);   
+        let num = 0;
+        for(let i = 0; i < this.values.length; i++){
+            num += this.values[i];    
+        }
+        return num;
     }
 }
+
+/*
+ * Implement a Polygon class with the following properties:
+ * 1. A constructor that takes an array of integer side lengths.
+ * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
+ */
 
 
 const rectangle = new Polygon([10, 20, 10, 20]);
