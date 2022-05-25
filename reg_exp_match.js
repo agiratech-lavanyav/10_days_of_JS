0609bad631d6f9aae4477889297b7f88dfabcc38
agiratech-lavanyav <lavanya.v@agiratech.com>
2022-05-25 11:24:37 +0530

    It matches a string that starts and ends with the same vowel

---
+++ reg_exp_match.js
@@ -0,0 +1,8 @@
+
+function regexVar() {
+    //  It must match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.', 
+    let re = RegExp(/^(Mr|Mrs|Ms|Dr|Er)(\.)([A-z])+$/);
+    return re;
+}
+// /^(Mr|Mrs|Ms|Dr|Er)(\.)([a-zA-Z])+$/
+
